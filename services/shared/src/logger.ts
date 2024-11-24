cb038c2c6de3b78ef6fca03554da2df5440ced08
Fajar Abdi Nugraha <fajar.nugraha@xtremax.com>
2024-11-24 21:19:50 +0700

    update loki logging the get log from docker host

--- services/shared/src/logger.ts
+++ services/shared/src/logger.ts
@@ -1,22 +1,8 @@
 import pino from 'pino';
-import type { LokiOptions } from 'pino-loki';
-
 
 export const logger = pino.default(
   {
     level: (process.env.LOKI_LOG_LEVEL || 'info'),
     name: process.env.SERVICE_NAME || 'default',
-  }, 
-  pino.transport<LokiOptions>({
-    target: "pino-loki",
-    options: {
-      labels: { 
-        service_name: process.env.SERVICE_NAME || 'default',
-      },
-      propsToLabels: ['level', 'pid'],
-      batching: true,
-      interval: +(process.env.LOKI_LOG_INTERVAL || '15'),
-      host: process.env.HTTP_LOKI_ENDPOINT!,
-    },
-  }),
+  },
 );
